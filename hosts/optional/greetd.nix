d5d552ab54e5a1cf2db69a37975a796b7ff3ddc5
Javier <jsanfe@amazon.com>
2025-05-16 13:18:25 +0200

    Fix home manager problems 35

--- hosts/optional/greetd.nix
+++ hosts/optional/greetd.nix
@@ -5,22 +5,17 @@
   ...
 }: let
   homeCfgs = config.home-manager.users;
-  #homeSharePaths = lib.mapAttrsToList (_: v: "${v.home.path}/share") homeCfgs;
-  homeSharePaths = lib.flatten [
-    (lib.mapAttrsToList (_: v: "${v.home.path}/share") homeCfgs)
-    "/home/sanfe/.nix-profile/share/wayland-sessions"
-    "/home/sanfe/.local/state/nix/profiles/profile/share/wayland-sessions"
-  ];
-  vars = ''XDG_DATA_DIRS="$XDG_DATA_DIRS:${lib.concatStringsSep ":" homeSharePaths}" GTK_USE_PORTAL=0 SESSION_DIRS=$SESSION_DIRS:/home/sanfe/.nix-profile/share/wayland-sessions:/home/sanfe/.local/state/nix/profiles/profile/share/wayland-sessions'';
+  homeSharePaths = lib.mapAttrsToList (_: v: "${v.home.path}/share") homeCfgs;
+  #vars = ''XDG_DATA_DIRS="$XDG_DATA_DIRS:${lib.concatStringsSep ":" homeSharePaths}" GTK_USE_PORTAL=0'';
 
-  sway-kiosk = command: "${lib.getExe pkgs.sway} --unsupported-gpu --config ${pkgs.writeText "kiosk.config" ''
-    output * bg #000000 solid_color
-    xwayland disable
-    input "type:touchpad" {
-      tap enabled
-    }
-    exec '${vars} ${command}; ${pkgs.sway}/bin/swaymsg exit'
-  ''}";
+  # sway-kiosk = command: "${lib.getExe pkgs.sway} --unsupported-gpu --config ${pkgs.writeText "kiosk.config" ''
+  #   output * bg #000000 solid_color
+  #   xwayland disable
+  #   input "type:touchpad" {
+  #     tap enabled
+  #   }
+  #   exec '${vars} ${command}; ${pkgs.sway}/bin/swaymsg exit'
+  # ''}";
 in {
   # users.extraUsers.greeter = {
   #   # For caching and such
@@ -31,7 +26,9 @@ in {
   services.displayManager.ly = {
     enable = true;
     settings = {
-	waylandsessions = "/home/sanfe/.nix-profile/share/wayland-sessions";
+	waylandsessions = "${lib.concatStringsSep ":" homeSharePaths}/wayland-sessions";
+	xsessions = "${lib.concatStringsSep ":" homeSharePaths}/xsessions";
+	asterisk = "0x2022";
     };
   };  
   
@@ -43,13 +40,13 @@ in {
   #   enable = true;
   #   settings.default_session.command = sway-kiosk (lib.getExe config.programs.regreet.package);
   # };
-
-  environment.persistence."/persist" = {
-    directories = [
-      { directory = "/var/lib/greeter-home"; }
-    ];
-  };
-  environment.variables.SESSION_DIRS = "/home/sanfe/.nix-profile/share/wayland-sessions:/home/sanfe/.local/state/nix/profiles/profile/share/wayland-sessions";
+  #
+  # environment.persistence."/persist" = {
+  #   directories = [
+  #     { directory = "/var/lib/greeter-home"; }
+  #   ];
+  # };
+  # environment.variables.SESSION_DIRS = "/home/sanfe/.nix-profile/share/wayland-sessions:/home/sanfe/.local/state/nix/profiles/profile/share/wayland-sessions";
 }
 
 
