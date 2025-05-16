4f1e9af894b389f50e20a20a3e0b179317dea42e
Javier <jsanfe@amazon.com>
2025-05-16 15:19:20 +0200

    Add ly display manager dinamic session detection

--- hosts/optional/greetd.nix
+++ hosts/optional/greetd.nix
@@ -6,24 +6,7 @@
 }: let
   homeCfgs = config.home-manager.users;
   homeSharePaths = lib.mapAttrsToList (_: v: "${v.home.path}/share") homeCfgs;
-
-  #vars = ''XDG_DATA_DIRS="$XDG_DATA_DIRS:${lib.concatStringsSep ":" homeSharePaths}" GTK_USE_PORTAL=0'';
-
-  # sway-kiosk = command: "${lib.getExe pkgs.sway} --unsupported-gpu --config ${pkgs.writeText "kiosk.config" ''
-  #   output * bg #000000 solid_color
-  #   xwayland disable
-  #   input "type:touchpad" {
-  #     tap enabled
-  #   }
-  #   exec '${vars} ${command}; ${pkgs.sway}/bin/swaymsg exit'
-  # ''}";
 in {
-  # users.extraUsers.greeter = {
-  #   # For caching and such
-  #   home = "/var/lib/greeter-home";
-  #   createHome = true;
-  # };
-
   services.displayManager.ly = {
     enable = true;
     settings = {
@@ -31,22 +14,14 @@ in {
 	xsessions = "${lib.concatStringsSep ":" homeSharePaths}/xsessions";
     };
   };  
-  
 
-  # programs.regreet = {
-  #   enable = true;
-  # };
-  # services.greetd = {
-  #   enable = true;
-  #   settings.default_session.command = sway-kiosk (lib.getExe config.programs.regreet.package);
-  # };
-  #
-  # environment.persistence."/persist" = {
-  #   directories = [
-  #     { directory = "/var/lib/greeter-home"; }
-  #   ];
-  # };
-  # environment.variables.SESSION_DIRS = "/home/sanfe/.nix-profile/share/wayland-sessions:/home/sanfe/.local/state/nix/profiles/profile/share/wayland-sessions";
+  environment.persistence = {
+    "${config.hostSpec.persistFolder}" = {
+      files = [
+        "/etc/ly/save.ini"
+      ];
+    };
+  };
 }
 
 
