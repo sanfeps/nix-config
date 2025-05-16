f8366673d9f3b6842d3e253a89926e4229c36fea
Javier <jsanfe@amazon.com>
2025-05-16 11:57:04 +0200

    Fix home manager problems 32

--- hosts/optional/greetd.nix
+++ hosts/optional/greetd.nix
@@ -31,51 +31,8 @@ in {
   services.displayManager.ly.enable = true;
 
   environment.etc."ly/config.ini".text = lib.mkForce ''
-    waylandsessions = /home/sanfe/.nix-profile/share/wayland-sessions
-
-    asterisk = 0x2022
-
-    # The number of failed authentications before a special animation is played... ;)
-    auth_fails = 10
-
-    # Background color id
-    bg = 0x00000000
-
-    # Blank main box background
-    # Setting to false will make it transparent
-    blank_box = true
-
-    # Border foreground color id
-    border_fg = 0x00FFFFFF
-
-    # Title to show at the top of the main box
-    # If set to null, none will be shown
-    box_title = null
-
-    # Erase password input on failure
-    clear_password = true
-
-    # Console path
-    console_dev = /dev/console
-
-    # Input box active by default on startup
-    # Available inputs: info_line, session, login, password
-    default_input = login
-
-    # Error background color id
-    error_bg = 0x00000000
-
-    # Error foreground color id
-    # Default is red and bold
-    error_fg = 0x01FF0000
-
-    # Foreground color id
-    fg = 0x00FFFFFF
-
-    # Remove main box borders
-    hide_borders = false
-
-  '';
+    sed -i 's|^waylandsessions=.*|waylandsessions=/home/sanfe/.nix-profile/share/wayland-sessions|' /etc/ly/config.ini
+     '';
 
   # programs.regreet = {
   #   enable = true;
