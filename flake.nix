34089dc2d011f749e0ff9ed9f1e16db57c8a4b1a
Javier <sanfelixguajardo@gmail.com>
2026-04-25 17:35:37 +0200

    Add noctalia-shell flake input

--- flake.nix
+++ flake.nix
@@ -32,6 +32,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    noctalia-shell = {
+      url = "github:noctalia-dev/noctalia-shell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     stylix = {
       url = "github:nix-community/stylix";
       inputs.nixpkgs.follows = "nixpkgs";
