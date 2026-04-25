044dc3fa917ecea1e451baa754352c0d9c473906
Javier <sanfelixguajardo@gmail.com>
2026-04-25 16:25:22 +0200

    Enable Xwayland satellite and screencast portal on niri

--- home/sanfe/features/desktop/niri/default.nix
+++ home/sanfe/features/desktop/niri/default.nix
@@ -8,12 +8,15 @@
     ../common/wayland-wm
   ];
 
-  home.packages = [pkgs.niri pkgs.swww];
+  home.packages = [pkgs.niri pkgs.awww pkgs.xwayland-satellite];
 
   xdg.portal = {
-    extraPortals = [pkgs.xdg-desktop-portal-gtk];
+    extraPortals = [pkgs.xdg-desktop-portal-gtk pkgs.xdg-desktop-portal-gnome];
     config.niri = {
       default = ["gtk"];
+      "org.freedesktop.impl.portal.ScreenCast" = ["gnome"];
+      "org.freedesktop.impl.portal.Screenshot" = ["gnome"];
+      "org.freedesktop.impl.portal.RemoteDesktop" = ["gnome"];
     };
   };
 
