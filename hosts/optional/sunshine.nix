af358014221001820d69f878c511af4e2025d135
sanfeps <sanfelixguajardo@gmail.com>
2026-05-07 01:01:10 +0200

    fix(sunshine): launch steam via wrapper script

--- hosts/optional/sunshine.nix
+++ hosts/optional/sunshine.nix
@@ -5,7 +5,24 @@
 }: let
   steam = "${config.programs.steam.package}/bin/steam";
   steamIcon = "${config.programs.steam.package}/share/icons/hicolor/256x256/apps/steam.png";
-  setsid = "${pkgs.util-linux}/bin/setsid";
+  launchSteamBigPicture = pkgs.writeShellScript "sunshine-steam-big-picture" ''
+    set -eu
+    cd "$HOME"
+    {
+      echo "[$(${pkgs.coreutils}/bin/date --iso-8601=seconds)] launch"
+      echo "PWD=$PWD"
+      echo "DISPLAY=$DISPLAY"
+      echo "WAYLAND_DISPLAY=$WAYLAND_DISPLAY"
+      echo "DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS"
+      echo "XDG_CURRENT_DESKTOP=$XDG_CURRENT_DESKTOP"
+    } >> /tmp/sunshine-steam-big-picture.log
+    exec ${pkgs.util-linux}/bin/setsid ${steam} steam://open/bigpicture
+  '';
+  closeSteamBigPicture = pkgs.writeShellScript "sunshine-steam-big-picture-close" ''
+    set -eu
+    cd "$HOME"
+    exec ${pkgs.util-linux}/bin/setsid ${steam} steam://close/bigpicture
+  '';
 in {
   services.sunshine = {
     enable = true;
@@ -17,12 +34,12 @@ in {
         {
           name = "Steam Big Picture";
           detached = [
-            "${setsid} ${steam} steam://open/bigpicture"
+            launchSteamBigPicture
           ];
           prep-cmd = [
             {
               do = "${pkgs.coreutils}/bin/true";
-              undo = "${setsid} ${steam} steam://close/bigpicture";
+              undo = closeSteamBigPicture;
             }
           ];
           auto-detach = true;
