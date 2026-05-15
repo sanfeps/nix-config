45a217931e398969b0d0e2c59af177d99b4f7dda
sanfeps <sanfelixguajardo@gmail.com>
2026-05-15 15:34:24 +0200

    fix(gh): drop declarative settings and persist config.yml

--- home/sanfe/features/cli/gh.nix
+++ home/sanfe/features/cli/gh.nix
@@ -2,13 +2,11 @@
   programs.gh = {
     enable = true;
     extensions = with pkgs; [gh-markdown-preview];
-    settings = {
-      version = "1";
-      git_protocol = "ssh";
-      prompt = "enabled";
-    };
   };
   home.persistence = {
-    "/persist".files = [".config/gh/hosts.yml"];
+    "/persist".files = [
+      ".config/gh/hosts.yml"
+      ".config/gh/config.yml"
+    ];
   };
 }
