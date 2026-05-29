0a29722ee7f6da742cbd0cc8b49f6cbf274234ed
sanfeps <sanfelixguajardo@gmail.com>
2026-05-29 20:21:53 +0200

    chore(nix): retain generations 7 days and auto-optimise store

--- hosts/common/core/nix.nix
+++ hosts/common/core/nix.nix
@@ -21,9 +21,9 @@ in {
     gc = {
       automatic = true;
       dates = "weekly";
-      # Keep the last 3 generations
-      options = "--delete-older-than +3";
+      options = "--delete-older-than 7d";
     };
+    optimise.automatic = true;
 
     # Add each flake input as a registry and nix_path
     # I think this options are needed when flake-registry is disabled
